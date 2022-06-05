9fe1384b5d9b4e8ea88c5c479f2e657ccd7a711e
Jerry Yan <792602257@qq.com>
2022-06-05 09:38:48 +0800

    留点坑之后再填

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-import sys
 import threading
 import subprocess
-from urllib.parse import urlparse
 
 from config.helper import config
 from handler.http_server import app
 from handler.utils import loop_queue
-from scripts import webdriver
+from browser.manager import BrowserManager
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1 or not urlparse(sys.argv[1]).scheme:
-        print('Invalid url provided, please check...')
-        sys.exit(1)
-    api_thread = threading.Thread(target=app.run, args=(config()["http"]["host"], config()["http"]["port"],))
-    api_thread.start()
     mitmproxy_process = subprocess.Popen([
-        config()["mitm"]["bin"], "-s", "./scripts/mitmproxy.py", "-q",
+        config()["mitm"]["bin"], "-s", "./proxy_script.py", "-q",
         "--listen-host", config()["mitm"]["host"], "--listen-port", str(config()["mitm"]["port"])
     ])
-    t = threading.Thread(target=webdriver.go, args=(sys.argv[1],))
-    t.start()
+    api_thread = threading.Thread(target=app.run, args=(config()["http"]["host"], config()["http"]["port"],))
+    api_thread.start()
+    manager = BrowserManager()
     queue_thread = threading.Thread(target=loop_queue)
     queue_thread.start()
     queue_thread.join()
