d48f7c772f6f82fdea07cf5739b7e44ce89e27c9
Jerry Yan <792602257@qq.com>
2022-06-05 22:38:31 +0800

    浏览器初始化内容

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import subprocess
 
 from config.helper import config
 from handler.http_server import app
-from browser.manager import BrowserManager
+from browser.manager import init_manager as init_browser_manager
 from output.manager import OutputManager
 
 if __name__ == '__main__':
@@ -13,9 +13,7 @@ if __name__ == '__main__':
     ])
     api_thread = threading.Thread(target=app.run, args=(config()["http"]["host"], config()["http"]["port"],))
     api_thread.start()
-    browser_manager = BrowserManager()
+    browser_manager = init_browser_manager()
     output_manager = OutputManager()
     output_manager.start_loop()
     api_thread.join()
-
-    
